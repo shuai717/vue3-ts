1b7963e46d810b14817cd32e5bd475fbe42c3e1d
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 <1824757520@qq.com>
2023-03-31 13:46:35 +0800

    update

--- src/components/RightMenu/index.vue
+++ src/components/RightMenu/index.vue
@@ -1,7 +1,8 @@
 <template>
   <span class="userName">欢迎您,admin</span>
   &nbsp;&nbsp;
-  <el-dropdown class="userInfo" trigger="click">
+  <!-- Menu 菜单项 -->
+  <el-dropdown class="userInfo" trigger="click"  @command="changeMenu">
     <el-avatar :size="40" >
       <el-icon>
         <User />
@@ -9,16 +10,60 @@
     </el-avatar>
     <template #dropdown>
       <el-dropdown-menu>
-        <el-dropdown-item>Action 1</el-dropdown-item>
-        <el-dropdown-item>Action 2</el-dropdown-item>
-        <el-dropdown-item>Action 3</el-dropdown-item>
-        <el-dropdown-item>Action 4</el-dropdown-item>
-        <el-dropdown-item>Action 5</el-dropdown-item>
+        <el-dropdown-item command="about">关于我们</el-dropdown-item>
+        <el-dropdown-item command="logout">
+          注销
+        </el-dropdown-item>
       </el-dropdown-menu>
     </template>
   </el-dropdown>
+  <!-- 注销确认框 -->
+  <el-dialog
+    v-model="dialogVisible"
+    :show-close="false"
+    width="500px"
+  >
+  <template #header="{ close, titleId, titleClass }">
+      <div class="my-header">
+        <h4 :id="titleId" :class="titleClass">系统提示!</h4>
+        <el-button type="danger" @click="close">
+          <el-icon class="el-icon--left"><CircleCloseFilled /></el-icon>
+          Close
+        </el-button>
+      </div>
+    </template>
+    <span>确定要退出当前用户吗?</span>
+    
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="handleClose">取消</el-button>
+        <el-button type="primary" @click="logout">
+          确定
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <script lang="ts" setup>
 import './index.less'
+import { useRouter,Router} from 'vue-router'
+let dialogVisible=ref(false)//注销确定框
+const router:Router=useRouter()
+
+const changeMenu=(key:string):void=>{
+  if(key === 'about'){
+  }else if(key === 'logout'){
+    dialogVisible.value=true
+  }
+}
+//确定退出
+const logout=():void=>{
+  router.replace('/login')
+  localStorage.clear()
+  handleClose()
+}
+const handleClose=():void=>{
+  dialogVisible.value=false
+}
 </script>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,18 +1,8 @@
 <template>
     <div class="homeWrap">
         home
-        <el-button @click="logout" type="primary"> 注销</el-button>
     </div>
 </template>
 <script setup lang="ts">
     import './home.less'
-    import { ref , reactive , toRaw } from 'vue'
-    import { useRouter,Router} from 'vue-router'
-    const router =useRouter()
-
-
-    
-    const logout=():void=>{
-        router.replace('/login')
-    }
 </script>
